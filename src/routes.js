75ffb9eefe5f6156ebfefec7d7b8d8ad02d95e0f
kleyson castro <kleysoncastro99@gmail.com>
2020-01-08 15:20:16 -0300

    chore: salva informações do upload de file no mongodb

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,21 @@
 const routes = require( 'express').Router();
 const muler = require('multer')
 const configMuler = require('./config/multer')
+const Post = require('./models/Post')
 
 
-  routes.post('/post', muler(configMuler).single('file'), (req, res) => {
-    console.log(req.file)
-    return res.json({ok: 'file'})
+  routes.post('/post', muler(configMuler).single('file'), async (req, res) => {
+    
+    const {originalname: name, size, filename: key} = req.file;
+
+    const post = await Post.create({
+      name, 
+      size,
+      key,
+      url: 'www.google.com'
+    })
+    
+    return res.json(post)
   })
 
 module.exports = routes;
